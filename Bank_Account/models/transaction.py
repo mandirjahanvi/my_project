6d344ac87d9576c1944e39401b024489f42679ff
mandirjahanvi <mahetajanu03@gmail.com>
2026-03-12 10:35:47 +0530

    update

--- Bank_Account/models/transaction.py
+++ Bank_Account/models/transaction.py
@@ -8,7 +8,7 @@ class transaction(models.Model):
     customer_id=fields.Many2one('customer.master',"Customer",domain="[('id','=',customer_details)]")
     amount=fields.Float('Amount')
     date=fields.Date('Date',default=lambda self:fields.Date.context_today(self))
-    transaction_type=fields.Selection([('credit','Credit'),('debit','Debit')],"Transaction Type")
+    transaction_type=fields.Selection([('credit','Deposit'),('debit','Withdraw')],"Transaction Type")
     customer_details=fields.Many2one('customer.master',related='bank_id.customer_id')
 
     @api.onchange('bank_id')
